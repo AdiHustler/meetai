3bc72bfa3c4a1c74a8c15402bc054451c2dc44f8
Aditya Thakur <aditya07thakur03@gmail.com>
2025-06-21 00:08:22 +0530

    04:authentication ui

---
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import React from "react";
+import {z} from "zod";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import { OctagonAlertIcon } from "lucide-react";
+import {zodResolver} from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
+import { Card, CardContent } from "@/components/ui/card";
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(1, "Password is required"),
+});
+
+export const SignInView = () => {
+  const router = useRouter();
+  const [error, setError] = React.useState<string | null>(null);
+  const [pending, setPending] = React.useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit =(data: z.infer<typeof formSchema>) => {
+    setError(null);
+    setPending(true);
+
+    authClient.signIn.email({
+      email: data.email,
+      password: data.password,
+      }, {
+      onError: ({error}) => {
+        setPending(false);
+        setError(error.message);
+        },
+      onSuccess: () => {
+        setPending(false);
+        router.push("/");
+        },
+      });
+    };
+  return (
+    <div className="text-l flex flex-col gap-6">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 md:p-8">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center justify-center">
+                  <h1 className="text-2xl font-bold text-center">
+                    Welcome back
+                  </h1>
+                  <p className="text-muted-foreground text-balance">
+                    Login to your account :)
+                  </p>
+                </div>
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input 
+                          type="email"
+                          placeholder="you@example.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  /> 
+                </div>
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input 
+                          type="password"
+                          placeholder="********" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                {!!error && (
+                  <Alert className="bg-destructive/10">
+                    <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                    <AlertTitle>{error}</AlertTitle>
+                  </Alert>
+                )}
+                <Button disabled={pending} type="submit" className="w-full">
+                  Sign In
+                </Button>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-top">
+                  <span className="bg-card text-muted-foreground relative z-10 px-3">
+                    Or continue with
+                  </span>
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <Button disabled={pending} variant="outline" className="w-full">
+                    <img src="/google.svg" alt="Google" className="h-4 w-4 mr-2" />
+                    Google
+                  </Button>
+                  <Button disabled={pending} variant="outline" className="w-full">
+                    <img src="/github.svg" alt="GitHub" className="h-4 w-4 mr-2" />
+                    GitHub
+                  </Button>
+                </div>
+                <div className="text-center text-sm text-muted-foreground">
+                  Don&apos;t have an account?{" "}
+                  <Link href="/sign-up" className="text-primary hover:underline">
+                    Sign Up
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </Form>
+
+          <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-4 items-center justify-center">
+            <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
+            <p className="text-2xl text-white font-semibold">Meet.AI</p>
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By signing in, you agree to our{" "}
+        <a href="/terms" className="text-primary hover:underline">
+          Terms of Service
+        </a>{" "}
+        and{" "}
+        <a href="/privacy" className="text-primary hover:underline">
+          Privacy Policy
+        </a>.
+      </div>
+    </div>
+  );
+}; 
