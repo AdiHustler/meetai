6054adaec784a568d19be67edc75b031a54bd2b5
Aditya Thakur <aditya07thakur03@gmail.com>
2025-07-13 16:12:43 +0530

    20: meeting page

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -10,6 +10,21 @@ import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
   // Define your procedures here
+  remove: protectedProcedure
+  .input(z.object({id:z.string()}))
+  .mutation(async({ctx, input})=>{
+      const [removedMeeting] = await db.delete(meetings).where(and(
+        eq(meetings.id, input.id),
+        eq(meetings.userId, ctx.auth.user.id),
+      )).returning();
+      if(!removedMeeting){
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+      return removedMeeting;
+    }),
   update: protectedProcedure.input(meetingsUpdateSchema).mutation(async({ctx, input})=>{
       const [updatedMeeting] = await db.update(meetings).set(input).where(and(
         eq(meetings.id, input.id),
@@ -34,7 +49,11 @@ export const meetingsRouter = createTRPCRouter({
   getOne: protectedProcedure.input(z.object({id:z.string()})).query(async ({ input, ctx }) => {
     const [existingMeeting] = await db.select({
       ...getTableColumns(meetings),
-    }).from(meetings).where(and(eq(meetings.id, input.id),eq(meetings.userId, ctx.auth.user.id),
+      agent: agents,
+      duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+    }).from(meetings)
+    .innerJoin(agents, eq(meetings.agentId, agents.id))
+    .where(and(eq(meetings.id, input.id),eq(meetings.userId, ctx.auth.user.id),
     ));
 
     if(!existingMeeting){
@@ -65,14 +84,7 @@ export const meetingsRouter = createTRPCRouter({
       const data = await db.select(
         {
         ...getTableColumns(meetings),
-        agent: {
-      id: agents.id,
-      name: agents.name,
-      userId: agents.userId,
-      createdAt: agents.createdAt,
-      updatedAt: agents.updatedAt,
-      // instructions: agents.instructions, // <-- DO NOT include this if it doesn't exist
-    },
+        agent: agents,
         duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
       }).from(meetings)
         .innerJoin(agents, eq(meetings.agentId, agents.id))
